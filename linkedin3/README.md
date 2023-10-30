255f08059510cb9510051efb8c3fe58d2d179822
gvsqzn <gvsqzn@gmail.com>
2023-10-30 03:10:04 -0300

    TEST-NO PESCAR

---
+++ linkedin3/README.md
@@ -0,0 +1,3 @@
+# linkedin3
+
+A new Flutter project.
---
+++ linkedin3/android/app/src/main/kotlin/com/example/linkedin3/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.linkedin3
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
